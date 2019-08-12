a43aa9825fc7545a3fefd4628bad75d7226c5646
Elite <elite@empireminecraft.com>
2019-08-12 18:53:52 -0400

    Add commands file | Fixes queueing

---
+++ commands.md
@@ -0,0 +1,28 @@
+# Commands
+
+## Key
+| Symbol     | Meaning                    |
+| ---------- | -------------------------- |
+| (Argument) | This argument is optional. |
+
+## Player
+| Commands   | Arguments                            | Description                                                                   |
+| ---------- | ------------------------------------ | ----------------------------------------------------------------------------- |
+| Clear      | <none>                               | Removes all currently queued songs.                                           |
+| Disconnect | <none>                               | Remove the bot from its current voice channel.                                |
+| Move       | (The ID of any valid voice channel.) | Move bot to the current voice channel or to a specified voice channel via ID. |
+| Mute       | <none>                               | Mute bot, but keeps it playing.                                               |
+| Pause      | <none>                               | Pauses the current song.                                                      |
+| Play       | URL                                  | Play the song listed - If a song is already playing, it's added to a queue.   |
+| Restart    | <none>                               | Replays the current song from the beginning.                                  |
+| Resume     | <none>                               | Continues the last song (If one is still queued)                              |
+| Skip       | <none>                               | Skips the current song.                                                       |
+| Unmute     | <none>                               | Sets bot's volume back to previous level before it was muted.                 |
+| Volume     | Integer (0-100)                      | Adjust volume from range 0-100                                                |
+
+## utility
+| Commands | Arguments | Description         |
+| -------- | --------- | ------------------- |
+| help     | (Word)    | Display a help menu |
+| ping     | <none>    | Responds with pong! |
+
--- src/main/kotlin/services/Handler.kt
+++ src/main/kotlin/services/Handler.kt
@@ -6,39 +6,37 @@ import com.sedmelluq.discord.lavaplayer.tools.FriendlyException
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason
 import me.aberrantfox.kjdautils.api.annotation.Service
-import java.util.*
-import java.util.concurrent.BlockingDeque
-
-//TODO fix queueing - not storing next songs currently - might convert to List instead of BlockingDeque
+import java.util.concurrent.LinkedBlockingDeque
+import java.util.concurrent.BlockingQueue
 
 @Service
 class Handler(private val plugin:ManagerService) : AudioEventAdapter() {
-    private val queue: BlockingDeque<AudioTrack>? = null
+    private var queue: BlockingQueue<AudioTrack> = LinkedBlockingDeque<AudioTrack>()
 
     fun isQueueEmpty():Boolean = queue.isNullOrEmpty()
 
     fun queue(track:AudioTrack){
         if (!plugin.player.startTrack(track, true)) {
-            queue?.offer(track)
+            queue.offer(track)
         }
     }
 
-    fun clearQueue(): List<AudioTrack> {
-        val clearQueue = ArrayList<AudioTrack>()
-        queue?.drainTo(clearQueue)
-        return clearQueue
-    }
+    fun clearQueue() = queue.clear()
 
     fun skip(){
-        plugin.player.startTrack(queue?.poll(), false)
+        plugin.player.startTrack(queue.poll(), false)
     }
 
     private fun startNextTrack(noInterrupt: Boolean) {
-        val next = queue?.pollFirst()
+        val next = queue.poll()
 
         if (next != null) {
             if (!plugin.player.startTrack(next, noInterrupt)) {
-                queue?.addFirst(next)
+                queue.add(next)
+                println("Song added")
+            }else{
+                queue.put(next)
+                println("Song put")
             }
         } else {
             plugin.player.stopTrack()
@@ -55,6 +53,8 @@ class Handler(private val plugin:ManagerService) : AudioEventAdapter() {
 
     override fun onTrackStart(player:AudioPlayer, track:AudioTrack) {
         // A track started playing
+        //TODO add a config file, and store and pull associated text channel
+        println("${track.info.title} started playing")
     }
 
     override fun onTrackEnd(player:AudioPlayer, track:AudioTrack, endReason: AudioTrackEndReason) {
