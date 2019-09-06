123c95c8591d563aa345ad61a5dc1bc12472c144
Elite <mrcalebf@gmail.com>
2019-09-06 04:07:07 -0400

    Annotate PrefixLoader as a Service | Add a configuration init service

--- src/main/kotlin/io/github/cferg/musicbot/utility/PrefixLoader.kt
+++ src/main/kotlin/io/github/cferg/musicbot/utility/PrefixLoader.kt
@@ -1,8 +1,10 @@
 package io.github.cferg.musicbot.utility
 
 import io.github.cferg.musicbot.data.Configuration
+import me.aberrantfox.kjdautils.api.annotation.Service
 import me.aberrantfox.kjdautils.discord.Discord
 
+@Service
 class PrefixLoader(discord: Discord, configuration: Configuration) {
     init {
         discord.configuration.prefix = configuration.prefix
